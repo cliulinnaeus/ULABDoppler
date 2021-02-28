c292269d178bd9bdd4b0064cd8f445479f319d95
Andris Huang <andrewhz@berkeley.edu>
2021-02-28 14:05:27 -0800

    added filtering function for wavelength

--- file_reader.py
+++ file_reader.py
@@ -33,18 +33,70 @@ for file in all_files:
     wavelength_lst.append(wavelength)
     flux_lst.append(flux)
 
+def filtered_coord(wavelength, flux):
+    """
+    Input: unfilered lists of wavelength and flux; type == 2D array
+    Output: new lists of wavelength and flux s.t. new_lst = [[old1], [old2]] if a gap exists between [old1] and
+        [old2]; type == 2D array: new_wavelength, new_flux
+
+    Precondition: lists of wavelength and flux have the same size
+    """
+    assert len(wavelength) == len(flux), 'Coordinates have the same size'
+
+    new_wl = []
+    new_fx = []
+    i = 0
+    while i < len(wavelength):
+        indv_wl, indv_fx = [wavelength[i][0]], [flux[i][0]]
+        j = 1
+        while j < len(wavelength[i]):
+            if is_gap(indv_wl, wavelength[i][j]):
+                new_wl.append(indv_wl)
+                new_fx.append(indv_fx)
+                indv_wl, indv_fx = [wavelength[i][j]], [flux[i][j]]
+            else:
+                indv_wl.append(wavelength[i][j])
+                indv_fx.append(flux[i][j])
+            j += 1
+        new_wl.append(indv_wl)
+        new_fx.append(indv_fx)
+        i += 1
+    return new_wl, new_fx
+
+def is_gap(cur_lst, item):
+    """ 
+    Identify if a gap exists between the current list item and the next item based on the difference in mean.
+    If the difference in mean between the new next element and current element in wavelength is 10x bigger 
+    than the mean between the current element and the previous element, return True; False otherwise.
+
+    Precondition: cur_lst has at least one element
+    """
+    assert len(cur_lst) > 0, 'empty list'
+    
+    if len(cur_lst) == 1:
+        return False
+    last_elem = cur_lst[len(cur_lst)-1]
+    sec_last = cur_lst[len(cur_lst)-2]
+    if (10 * abs(last_elem - sec_last)) < abs(item - last_elem):
+        return True
+    return False
+
+new_wavelength, new_flux = filtered_coord(wavelength_lst, flux_lst)
 
 
 # file_name = make_file_name(root_dir, 1994, 7, 49552.340)
 #
 #
 # x, y = read_single_file(file_name)
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
-for w, f in zip(wavelength_lst, flux_lst):
+for w, f in zip(new_wavelength, new_flux):
     ax.clear()
     ax.plot(w, f)
-    plt.pause(0.01)
+    plt.pause(0.1)
+
+#print(new_wavelength)
 
 
 
