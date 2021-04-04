04b079f1820486b7531ff1182dbb4c3e88c18530
Che Liu <cliu88@berkeley.edu>
2021-04-03 21:38:50 -0700

    cleaned up the code, added plotting function

--- Simulator.py
+++ Simulator.py
@@ -2,101 +2,80 @@ import numpy as np
 import scipy as sp
 import math
 import copy
+import matplotlib.pyplot as plt
+
 
 class Star:
 
 
-    def __init__(self, inclination_angle, temp, radius, v_e):
+    def __init__(self, inclination_angle, temp, radius, v_e, num_of_patches):
+        spots_lat = np.array([np.pi/4])
+        spots_long = np.array([np.pi])
+        spots_radius = np.array([10])
+        spots_temp = np.array([3000])
+
+
+
         self.inclination_angle = inclination_angle
         self.temp = temp
         self.radius = radius
         self.v_e = v_e
-        self.I = None
-        self.num_latitudes = 0
-        self.zones = None
-
-    def _sort_into_bins(self):
-        arr = []
-        start_idx = 0
-        for z in self.zones:
-            arr.append(copy.deepcopy(self.I[start_idx: start_idx + z]))
-            start_idx = z
-        return arr
+        self.num_latitudes, self.zones = self.get_lats_and_zones(num_of_patches)
+        self.I = self.make_image_vector(num_of_patches, spots_lat, spots_long, spots_radius, spots_temp)
 
 
-    def add_sunspots(self, spots_lat, spots_long, spots_radius, spots_temp):
+    def add_sunspots(self, I, spots_lat, spots_long, spots_radius, spots_temp):
         num_latitudes = int(self.num_latitudes)
         zones = self.zones
 
         angles = np.zeros(num_latitudes)
         total_polar_angle = np.pi - (np.pi/2 - self.inclination_angle)
-        delta_angle = total_polar_angle / num_latitudes 
+        delta_angle = total_polar_angle / num_latitudes
 
         #define the latitudes we have in terms of angles
         for i in range(1, num_latitudes+1):
             polar_angle = delta_angle*i
             angles[i-1] = np.pi/2 - polar_angle
-            
+
         for x in range(len(spots_lat)):
             init_latitude = spots_lat[x]
             init_longitude = spots_long[x]
 
             i = math.floor((np.pi/2 - init_latitude) / delta_angle)
-           
+
             delta_phi = 2*np.pi / zones[i]
 
             j = math.floor((init_longitude) / delta_phi)
 
             index = int(np.sum(self.zones[0:i])) + j
 
-            self.I[index] = spots_temp[x]
-
-
-
-            
-
-
-
-
-
-            
-
-
-
-            
-        
-
-
-        
-
-
- 
-        return None
+            I[index] = spots_temp[x]
+        return I
 
     def get_lats_and_zones(self, n):
 
         area_per_patch = self.calc_zone_area(self.radius, self.inclination_angle, n)
         length_zone = np.sqrt(area_per_patch)
-        
+
         #calculate how many "latitudes" on sphere
         total_polar_angle = np.pi - (np.pi/2 - self.inclination_angle)
-        num_latitudes = math.floor((total_polar_angle * self.radius) / length_zone)
+        num_latitudes = int(math.floor((total_polar_angle * self.radius) / length_zone))
 
         #calculate how many zones for each latitude
-        delta_angle = total_polar_angle / num_latitudes 
+        delta_angle = total_polar_angle / num_latitudes
 
         zones = np.zeros(num_latitudes)
 
         for i in range(1, len(zones)+1):
-            polar_angle = delta_angle*i 
+            polar_angle = delta_angle*i
             num_zones = (2 * np.pi * self.radius * np.sin(polar_angle)) / length_zone
 
-            zones[i-1] = math.floor(num_zones)
+            zones[i-1] = int(math.floor(num_zones))
 
         self.num_latitudes = num_latitudes
         self.zones = zones
-        
-        return num_latitudes, zones          
+
+        return num_latitudes, zones
 
 
     def get_stellar_disk(self, phase):
@@ -107,21 +86,48 @@ class Star:
 
         total_area = (4 * np.pi * radius**2) - (2 * np.pi * radius**2)*(1 - np.sin(inclination_angle))
         area_per_patch = total_area/n
-        
+
         return area_per_patch
 
 
     def make_image_vector(self, n, spots_lat, spots_long, spots_radius, spots_temp):
-        num_latitudes, zones = self.get_lats_and_zones(n)
-        num_zones = np.sum(zones)
-        self.I = np.full( num_zones , self.temp) 
-        self.add_sunspots(spots_lat, spots_long, spots_radius, spots_temp)
-        
+        num_latitudes = self.num_latitudes
+        num_zones = int(np.sum(self.zones))
+        I = np.full(num_zones, self.temp)
+        I = self.add_sunspots(I, spots_lat, spots_long, spots_radius, spots_temp)
+        return I
+
+    def _sort_into_bins(self):
+        arr = []
+        start_idx = 0
+        for z in self.zones:
+            z = int(z)
+            arr.append(copy.deepcopy(self.I[start_idx: start_idx + z]))
+            start_idx = z
+        return arr
+
+    def plot_on_sphere(self):
+        I = self.I
+        n = len(I)
+        inclination_angle = self.inclination_angle
+        num_latitudes = self.num_latitudes
+        zones = self.zones
+        width = int(max(zones))
+        height = int(num_latitudes)
+        map = np.zeros((height, width))
+        bins = self._sort_into_bins()
+        for idx, bin in enumerate(bins):
+            start_col = (width - len(bin)) // 2
+            map[idx][start_col : start_col + len(bin)] = bin
+            # map[idx][0 : len(bin)] = bin
+        plt.imshow(map, cmap='hot')
+        plt.show()
 
 
 
-s = Star(np.pi/2,5000,3,4)
+s = Star(np.pi/2, 5000, 3e6, 4, 1000)
+# s.make_image_vector(2000, np.array([0]), np.array([0]),1,np.array([2000]))
 
-s.make_image_vector(2000, np.array([0]), np.array([0]),1,np.array([2000]))
+s.plot_on_sphere()
 
-print(s.I) 
+# print(s.I)
