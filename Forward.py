2521106d05e7f77d0e5c4798a40c13fb5deb680c
Andris Huang <andrewhz@berkeley.edu>
2021-04-18 22:35:21 -0700

    wrote black body radiation matrix

---
+++ Forward.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from scipy import constants
+from scipy.integrate import quad
+
+h = constants.Planck
+c = constants.c
+k = constants.k
+inf = np.inf
+
+def black_body(wavelength, temperature):
+    """
+    Input: wavelength, temperature
+    Output: intensity calculated by B(l, T)
+    """
+    l, T = wavelength, temperature
+    return 2*h*(c**2)/(l**5)*1/((np.exp(h*c/(l*k*T)))-1)
+
+def get_temperature(I):
+    """
+    Input: Image vector I
+    Output: Temperature of each element(i) in I, T(i); type = 1D_array
+    """
+    return I
+
+
+def black_body_matrix(temp, frac=[1, 1]):
+    """
+    Input: wavelength, temperature; type: np.array
+    Output: matrix constructed using black body radiation
+    """
+    B = []
+    for f in frac:
+        Bi = []
+        for T in temp:
+            B_ij = np.array([quad(black_body, 0, inf, args=(T))[0]])
+            Bi.append(B_ij*f)
+        B.extend(Bi)
+    B_mat = np.array(B)
+    B_mat.reshape(len(temp), len(frac))
+    return np.array(B_mat)
+
+def doppler_shift(v_rot):
+    """
+    Input: rotational velocity of the star
+    Output: the doppler shift fraction of wavelength using equation $v_rot/c = lambda / lambda_0$
+    """
+    return v_rot / c + 1
+
+print(black_body_matrix([1]))
